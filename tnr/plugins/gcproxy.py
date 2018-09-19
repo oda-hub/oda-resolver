f2e3ca9a92795fc55f26121c55718c8df94a147d
Volodymyr <vladimir.savchenko@gmail.com>
2018-09-19 18:16:34 +0200

    working proxy

--- tnr/plugins/gcproxy.py
+++ tnr/plugins/gcproxy.py
@@ -1,9 +1,17 @@
 from tnr.resolvers import Resolver
 
+import os
 import requests
 from requests.auth import HTTPBasicAuth
 
 class GCProxyResolver(Resolver):
+
+    @property
+    def secret(self):
+        return open(os.environ.get("GCPROXY_SECRET_LOCATION","secret")).read().strip()
+
     def resolve(self,name):
-        return requests.get("http://134.158.75.161/cat/grbcatalog/api/v1.1/"+name,auth=HTTPBasicAuth("integral",open("secret").read().strip())).json()
+        return requests.get("http://134.158.75.161/cat/grbcatalog/api/v1.1/"+name,
+                            auth=HTTPBasicAuth("integral", self.secret),
+                        ).json()
 
